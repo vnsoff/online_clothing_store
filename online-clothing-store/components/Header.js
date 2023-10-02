c19f17ade136032eab1dbc227c82ae50f47de07e
Vanessa Felix Christoff <vfchristoff9@gmail.com>
2023-10-02 06:29:17 -0300

    Navbar structure

--- online-clothing-store/components/Header.js
+++ online-clothing-store/components/Header.js
@@ -1,7 +1,7 @@
-// components/Header.js
-
-import Link from "next/link";
-import "tailwindcss/tailwind.css"; // Import Tailwind CSS
+import Link from 'next/link';
+import Image from 'next/image';
+import { ShoppingCart, User, Heart } from 'feather-icons'; // Import the Feather Icons
+import 'tailwindcss/tailwind.css'; // Import Tailwind CSS
 
 function Header() {
   return (
@@ -10,10 +10,70 @@ function Header() {
         <div className="text-2xl font-bold mb-4">
           <Link href="/">Emporium</Link>
         </div>
-        <nav className="flex items-center justify-center space-x-4">
-          <Link href="/">Home</Link>
-          <Link href="/women">Women</Link>
-          <Link href="/men">Men</Link>
+        <nav className="flex items-center justify-center">
+          <div className="flex space-x-20 items-center">
+            <Link href="/">Home</Link>
+            <Link href="/trends">Trends</Link>
+            <Link href="/new">New</Link>
+            <Link href="/women">Women</Link>
+            <Link href="/men">Men</Link>
+            <Link href="/plus_size">Plus Size</Link>
+            <Link href="/brands">Brands</Link>
+            <div className="relative text-gray-600">
+              <input
+                type="search"
+                name="search"
+                placeholder="Search products"
+                className="bg-white h-10 px-5 pr-10 rounded-full text-sm focus:outline-none"
+              />
+            <button type="submit" className="absolute right-0 top-0 mt-3 mr-4 flex items-center">
+              {/* Custom Search Icon */}
+              <Image
+                src="/images/search-icon.png"
+                alt="Search"
+                width={24}
+                height={24}
+                className="w-6 h-6 ml-auto" // Use ml-auto to push the icon to the right
+              />
+            </button>
+            </div>
+            {/* Cart Button */}
+            <Link href="/cart" legacyBehavior>
+              <a className="text-gray-600">
+                <Image
+                  src="/images/cart-icon.png"
+                  alt="Cart"
+                  width={24}
+                  height={24}
+                  className="w-6 h-6"
+                />
+              </a>
+            </Link>
+            {/* Login/Profile Button */}
+            <Link href="/profile" legacyBehavior>
+              <a className="text-gray-600">
+                <Image
+                  src="/images/profile-icon.png"
+                  alt="Profile"
+                  width={24}
+                  height={24}
+                  className="w-6 h-6"
+                />
+              </a>
+            </Link>
+            {/* Favorites Button */}
+            <Link href="/favorites" legacyBehavior>
+              <a className="text-gray-600">
+                <Image
+                  src="/images/favorites-icon.png"
+                  alt="Favorites"
+                  width={24}
+                  height={24}
+                  className="w-6 h-6"
+                />
+              </a>
+            </Link>
+          </div>
         </nav>
       </div>
     </header>
