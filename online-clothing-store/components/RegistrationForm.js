56c360e4d8bfa27df5063eae7df186b3f2184d14
Vanessa <vfchristoff9@gmail.com>
2023-10-30 07:40:50 -0300

    Registration and Login posts

--- online-clothing-store/components/RegistrationForm.js
+++ online-clothing-store/components/RegistrationForm.js
@@ -2,55 +2,45 @@
 import React from 'react';
 import { useState } from 'react';
 import Axios from 'axios';
+import { response } from 'express';
 
 function RegistrationForm () {
-/* class RegistrationForm extends Component { */
-/*   constructor() {
-    super();
-    this.state = {
-      username: '', // Changed 'name' to 'username'
-      email: '',    // Changed 'login' to 'email'
-      password: '',
-    };
-  }
-
-  handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    fetch('/api/register', {
-      method: 'POST',
-      body: JSON.stringify(this.state),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((response) => {
-        if (response.status === 201) {
-          alert('Registration successful!');
-        } else {
-          alert('Registration failed. Please try again.');
-        }
-        return response.json();
-      })
-      .then((data) => {
-        // You can further process the response data if needed.
-        console.log(data);
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-        alert('An error occurred. Please try again.');
-      });
-  };
- */
 
   const[email,setEmail] = useState("");
   const[username,setUsername]  = useState("");
   const[password,setPassword] = useState("");
   const[loginStatus,setLoginStatus] = useState("");
   const[registerStatus,setRegisterStatus] = useState("");
+
+  const register = (e) => {
+    e.preventDefault();
+    Axios.post("https://localhost:3001/register", {
+      email: email,
+      username: username,
+      password: password,
+    }).then((response) => {
+      if (response.data.message) {
+        setRegisterStatus(response.data.message);
+      }
+      else {
+        setRegisterStatus("ACCOUNT CREATED SUCCESSFULLY");
+      }
+    })
+  }
+  const login = (e) => {
+    e.preventDefault();
+    Axios.post("https://localhost:3001/login", {
+      username: username,
+      password: password,
+    }).then((response) => {
+      if (response.data.message) {
+        setLoginStatus(response.data.message);
+      }
+      else {
+        setLoginStatus(response.data[0].email);
+      }
+    })
+  }
 
     return (
       <div className='container'>
@@ -68,9 +58,9 @@ function RegistrationForm () {
           <form>
           <h4>Register</h4>
           <label htmlFor="email">Email Adress</label>
-          <input className="textInput" type="text" name="email"placeholder="Enter your Email Adress" required/>
+          <input className="textInput" type="text"onChange={(e)=> {setEmail(e.target.value)}} name="email"placeholder="Enter your Email Adress" required/>
           <label htmlFor="username">Username</label>
-          <input className="textInput" type="text" name="username"placeholder="Enter your username" required/>
+          <input className="textInput" type="text" onChange={(e)=> {setUsername(e.target.value)}} name="username"placeholder="Enter your username" required/>
           <label htmlFor="password">Password</label>
           <input className="textInput" type="text" name="password"placeholder="Enter your password" required/>
           <input className='button' type='submit' onClick={register} value="Login"/>
